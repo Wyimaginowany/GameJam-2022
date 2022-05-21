a3c0de5b958de1cffb5fad4c127e37b53d6b3ea7
Wyimaginowany <goralczyk.jakub@wp.pl>
2022-05-21 23:23:51 +0200

    checking if is on wall

--- GameJam_2022/Assets/PlayerMovement.cs
+++ GameJam_2022/Assets/PlayerMovement.cs
@@ -8,9 +8,14 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float movementSpeed = 4f;
     [SerializeField] float jumpForce = 2f;
     [SerializeField] LayerMask groundLayer;
+    [SerializeField] LayerMask wallLayer;
+
+    [SerializeField] bool isOnWall;
+    [SerializeField] bool isOnGround;
 
     PlayerControls playerControls;
     Vector2 movementVector;
+    float facingDirection;
     BoxCollider2D collider;
     Rigidbody2D rigidBody;
 
@@ -30,9 +35,11 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         movementVector = playerControls.Player.Move.ReadValue<Vector2>();
-        Debug.Log(playerControls.Player.Move.ReadValue<Vector2>());
-
         rigidBody.velocity = new Vector2(movementVector.x * movementSpeed, rigidBody.velocity.y);
+
+        UpdateFacingDirection();
+        isOnGround = isGrounded();
+        isOnWall = onWall();
     }
 
     private void Jump(InputAction.CallbackContext context)
@@ -46,6 +53,25 @@ public class PlayerMovement : MonoBehaviour
         RaycastHit2D groundHit = Physics2D.BoxCast(
             collider.bounds.center, collider.bounds.size, 0, Vector2.down, 0.01f, groundLayer);
         return groundHit.collider != null;
+    }
+
+    private bool onWall()
+    {
+        RaycastHit2D wallHit = Physics2D.BoxCast(
+        collider.bounds.center, collider.bounds.size, 0, new Vector2(facingDirection, 0), 0.01f, groundLayer);
+        return wallHit.collider != null;
+    }
+
+    private void UpdateFacingDirection()
+    {
+        if (movementVector.x > 0.01f)
+        {
+            facingDirection = 1;
+        }
+        else if (movementVector.x < 0.01f)
+        {
+            facingDirection = -1;
+        }
     }
 
 }
