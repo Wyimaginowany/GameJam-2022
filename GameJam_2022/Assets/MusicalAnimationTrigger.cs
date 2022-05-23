f9024a1be2b763a4d190eccc73c3a8b3518f255d
Wyimaginowany <goralczyk.jakub@wp.pl>
2022-05-23 16:52:39 +0200

    music sync with animations

---
+++ GameJam_2022/Assets/MusicalAnimationTrigger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using SonicBloom.Koreo;
+using System;
+
+public class MusicalAnimationTrigger : MonoBehaviour
+{
+
+    public Animation animCom;
+
+    [EventID]
+    public string eventID;
+
+    void Awake()
+    {
+        Koreographer.Instance.RegisterForEvents(eventID, OnAnimationTrigger);
+    }
+
+    private void OnAnimationTrigger(KoreographyEvent koreoEvent)
+    {
+        animCom.Stop();
+        animCom.Play();
+    }
+}
