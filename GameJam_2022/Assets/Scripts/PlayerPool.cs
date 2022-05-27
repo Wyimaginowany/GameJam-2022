145f04f5b2260229c523d859337433b4b86d3083
Wyimaginowany <goralczyk.jakub@wp.pl>
2022-05-27 14:15:07 +0200

    player health UI

--- GameJam_2022/Assets/Scripts/PlayerPool.cs
+++ GameJam_2022/Assets/Scripts/PlayerPool.cs
@@ -1,21 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerPool : MonoBehaviour
 {
 
     [SerializeField] GameObject[] players;
+    [SerializeField] TextMeshProUGUI playerLifesText;
+    [SerializeField] GameObject can;
 
     int currentPlayer = 0;
+    int playerLifes;
 
     private void Start()
     {
+        playerLifes = players.Length - 1;
         SpawnNextPlayer();
     }
 
     public void SpawnNextPlayer()
     {
+        if (playerLifes == 0) can.SetActive(false);
+        else
+        {
+            playerLifesText.text = (playerLifes).ToString() + " x";
+        }
+
         if (currentPlayer <= players.Length - 1)
         {
             players[currentPlayer].SetActive(true);
@@ -25,6 +36,7 @@ public class PlayerPool : MonoBehaviour
             Debug.Log("Show end screen");
         }
 
+        playerLifes--;
         currentPlayer++;
     }
 
